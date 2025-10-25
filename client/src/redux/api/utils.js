e9aa895b9389fe5cddaf8c473313b0842b86b5eb
palash sahu <73164304+shanupalash@users.noreply.github.com>
2025-10-25 16:40:05 +0530

    Set default API URL for utils.js

--- client/src/redux/api/utils.js
+++ client/src/redux/api/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL || "https://alumniecho-university-alumni-connect.onrender.com" ;
 const ADMIN_URL = `${BASE_URL}/admin`;
 
 const authInterceptor = (req) => {
